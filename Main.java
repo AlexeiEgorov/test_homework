efc4e506627d1aa7e988fa9f4e23848798636eee
Alexei Egorov <ZibbidiZibbidiBom@gmail.com>
2023-04-13 21:32:55 +0300

    And Here We Go!

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
 	
 	public static void main(String[] args) {
-	System.out.println("Zibbidi Zibbidi Bom!")
+		System.out.println("Zibbidi Zibbidi Bom!");
+		System.out.println("And Here We Go!");
 	}
 	
 }
